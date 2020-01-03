f94221f87c903f854a93d0f445b103f7e20870e8
Mohamad Barbar <mbarbar@runbox.com>
2020-01-03 20:06:17 +1100

    Make gather.sh more user friendly.
    * Better output file naming.
    * Use an out dir.

--- gather.sh
+++ gather.sh
@@ -2,14 +2,17 @@
 
 svf=$1
 analysis=$2
+outdir=$3
 
-if [ "$#" -ne 2 ]; then
-    echo "usage: $0 svf-binary analysis-type"
+if [ "$#" -ne 3 ]; then
+    echo "usage: $0 svf-binary analysis-type out-dir"
     exit 1
 fi
 
+mkdir "$outdir"
+
 for f in *.bc; do
-    command="/usr/bin/time -o $f.time $svf -$analysis $f > $f.svf"
+    command="/usr/bin/time -o $outdir/$f.$analysis.time $svf -$analysis $f > $outdir/$f.$analysis.svf"
     echo "Running: '$command'"
     eval $command
 done
