d3ff4b6c6688b52b51d683200197ee3b87541431
Mohamad Barbar <mbarbar@runbox.com>
2020-04-04 16:30:30 +1100

    Make #runs dynamic.

--- gather.sh
+++ gather.sh
@@ -3,15 +3,16 @@
 svf=$1
 analysis=$2
 outdir=$3
+runs=$4
 
-if [ "$#" -ne 3 ]; then
-    echo "usage: $0 svf-binary analysis-type out-dir"
+if [ "$#" -ne 4 ]; then
+    echo "usage: $0 svf-binary analysis-type out-dir #runs"
     exit 1
 fi
 
 mkdir "$outdir"
 
-for run in `seq 1 10`; do
+for run in `seq 1 $runs`; do
     echo "======== RUN $run ========"
     for f in *.bc; do
         command="/usr/bin/time -o $outdir/$f.$analysis.$run.time $svf -$analysis $f > $outdir/$f.$analysis.$run.svf"
