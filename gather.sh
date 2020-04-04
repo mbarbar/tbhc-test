e650fe435beeb6a1ed17e72ddefe12bd1b917e37
Mohamad Barbar <mbarbar@runbox.com>
2020-04-04 16:31:11 +1100

    Update gather.sh analysis arg passing.

--- gather.sh
+++ gather.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 svf=$1
-analysis=$2
+analysis_args=$2
 outdir=$3
 
 if [ "$#" -ne 3 ]; then
-    echo "usage: $0 svf-binary analysis-type out-dir"
+    echo "usage: $0 svf-binary analysis-args out-dir"
     exit 1
 fi
 
@@ -14,7 +14,7 @@ mkdir "$outdir"
 for run in `seq 1 10`; do
     echo "======== RUN $run ========"
     for f in *.bc; do
-        command="/usr/bin/time -o $outdir/$f.$analysis.$run.time $svf -$analysis $f > $outdir/$f.$analysis.$run.svf"
+        command="/usr/bin/time -o $outdir/$f.$run.time $svf $analysis_args $f > $outdir/$f.$run.svf"
         echo "Running: '$command' for run #$run"
         eval $command
     done
