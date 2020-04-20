1c747af5838979862057758a22623140b2d25cb4
Mohamad Barbar <mbarbar@runbox.com>
2020-04-20 20:26:29 +1000

    Use option for gather_all

--- gather_all.sh
+++ gather_all.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-./gather.sh ~/clone/svf/Release-build/bin/wpa '-fspta'                  fspta-1f0c-na     10
-./gather.sh ~/clone/svf/Release-build/bin/wpa '-fstbhc'                 fstbhc-1f0c-nr-na 10
-./gather.sh ~/clone/svf/Release-build/bin/wpa '-fstbhc -tbhc-all-reuse' fstbhc-1f0c-ar-na 10
+./gather.sh $1 '-fspta'                  fspta-1f0c-na     10
+./gather.sh $1 '-fstbhc'                 fstbhc-1f0c-nr-na 10
+./gather.sh $1 '-fstbhc -tbhc-all-reuse' fstbhc-1f0c-ar-na 10
 
-./gather.sh ~/clone/svf/Release-build/bin/wpa '-fspta -ctir-alias-eval'                  fspta-1f0c-wa     1
-./gather.sh ~/clone/svf/Release-build/bin/wpa '-fstbhc -ctir-alias-eval'                 fstbhc-1f0c-nr-wa 1
-./gather.sh ~/clone/svf/Release-build/bin/wpa '-fstbhc -tbhc-all-reuse -ctir-alias-eval' fstbhc-1f0c-ar-wa 1
+./gather.sh $1 '-fspta -ctir-alias-eval'                  fspta-1f0c-wa     1
+./gather.sh $1 '-fstbhc -ctir-alias-eval'                 fstbhc-1f0c-nr-wa 1
+./gather.sh $1 '-fstbhc -tbhc-all-reuse -ctir-alias-eval' fstbhc-1f0c-ar-wa 1
