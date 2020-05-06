a9194bb23856d1901259c4f8f253818838e411f8
Mohamad Barbar <mbarbar@runbox.com>
2020-05-06 22:09:47 +1000

    Use unique output dirs in benchmark.sh.

--- benchmark.sh
+++ benchmark.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-# Two FSPTA runs: one for time (ptr-only SVFG), one for alias testing (full SVFG).
-./gather.sh $1 '-fspta'                                   sparse-time     sparse-time      0
-./gather.sh $1 '-fspta                  -ctir-alias-eval' sparse-alias    sparse-alias    25
-./gather.sh $1 '-fstbhc                 -ctir-alias-eval' typeclone       typeclone       50
-./gather.sh $1 '-fstbhc -tbhc-all-reuse -ctir-alias-eval' typeclone-reuse typeclone-reuse 75
+time=`date +%s`
 
-./table.awk sparse-time/* sparse-alias/* typeclone/* typeclone-reuse/*
+# Two FSPTA runs: one for time (ptr-only SVFG), one for alias testing (full SVFG).
+./gather.sh $1 '-fspta'                                   sparse-time-$time     sparse-time      0
+./gather.sh $1 '-fspta                  -ctir-alias-eval' sparse-alias-$time    sparse-alias    25
+./gather.sh $1 '-fstbhc                 -ctir-alias-eval' typeclone-$time       typeclone       50
+./gather.sh $1 '-fstbhc -tbhc-all-reuse -ctir-alias-eval' typeclone-reuse-$time typeclone-reuse 75
+
+./table.awk sparse-time-$time/* sparse-alias-$time/* typeclone-$time/* typeclone-reuse-$time/*
